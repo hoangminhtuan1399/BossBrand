6b49488a54fd440cf4c025f959a5c78ffe079847
Nhom3-DuongMinhHieu <mhd29011999@gmail.com>
2022-02-10 20:34:31 +0700

    feat: address

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,18 +1,36 @@
-let add = document.querySelector(".add"),
-    minus = document.querySelector(".minus"),
-    number = document.querySelector(".number")
+const apiUrl = 'https://provinces.open-api.vn/api';
+
+const add = document.querySelector('.add');
+const minus = document.querySelector('.minus');
+const number = document.querySelector('.number');
 
 add.onclick = () => {
-    number.value = parseInt(number.value) + 1
-    if(number.value > 4) {
-        number.value = 1
+    number.value = parseInt(number.value) + 1;
+    if (number.value > 4) {
+        number.value = 1;
     }
-    
-}
+};
 
 minus.onclick = () => {
-    number.value = parseInt(number.value) - 1
-    if(number.value < 0) {
-        number.value = 4
+    number.value = parseInt(number.value) - 1;
+    if (number.value < 0) {
+        number.value = 4;
     }
-}
+};
+
+const localPicker = new LocalPicker({
+    province: 'provinces',
+    district: 'districts',
+    ward: 'wards'
+});
+
+const shop = document.querySelector('.shop-wrapper');
+
+document.querySelector('[name="provinces"]').addEventListener('change', (e) => {
+    if (e.target.value === '1') {
+        shop.style.display = 'block';
+    } else {
+        shop.style.display = 'none';
+    }
+})
+
